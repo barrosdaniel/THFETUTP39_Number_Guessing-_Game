bd94d636835c62471875335daf6f57fc98eb51a6
Daniel Barros <barros.daniel@outlook.com>
2019-07-02 09:01:10 +0800

    feature: Add second change to guess number

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 var correctGuess = false;
-var randomNumber = Math.floor(Math.random() * 6 ) + 1;
-var guess = prompt('I am thinking of a number between 1 and 6. What is it?');
-if (parseInt(guess) === randomNumber ) {
+var randomNumber = Math.floor(Math.random() * 6) + 1;
+var guess = parseInt(prompt('I am thinking of a number between 1 and 6. What is it?'));
+if (guess === randomNumber) {
   correctGuess = true;
-} 
-if ( correctGuess ) {
-    document.write('<p>You guessed the number!</p>');
+} else if (guess < randomNumber) {
+  var guessMore = parseInt(prompt('Try again. The number I am thinking of is more than ' + guess + '.'));
+  if (guessMore === randomNumber) {
+    correctGuess = true;
+  }
+} else if (guess > randomNumber) {
+  var guessMore = parseInt(prompt('Try again. The number I am thinking of is less than ' + guess + '.'));
+  if (guessMore === randomNumber) {
+    correctGuess = true;
+  }
+}
+
+if (correctGuess) {
+  document.write('<p>You guessed the number!</p>');
 } else {
-    document.write('<p>Sorry. The number was ' + randomNumber + '.</p>');
+  document.write('<p>Sorry. The number was ' + randomNumber + '.</p>');
 }
